2e86b43546261e6c25f56800937984185a8df97c
tfdx29 <eyeofsouran69@gmail.com>
2022-06-15 11:10:30 +0545

    Few Tweaks Added

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-# Next.js + Tailwind CSS Example
+# Next.js with Typescript + Tailwind CSS
 
-This example shows how to use [Tailwind CSS](https://tailwindcss.com/) [(v3.0)](https://tailwindcss.com/blog/tailwindcss-v3) with Next.js. It follows the steps outlined in the official [Tailwind docs](https://tailwindcss.com/docs/guides/nextjs).
-
-## Deploy your own
-
-Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-tailwindcss)
-
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss&project-name=with-tailwindcss&repository-name=with-tailwindcss)
-
-## How to use
-
-Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init) or [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/) to bootstrap the example:
+## To Run Locally
 
 ```bash
-npx create-next-app --example with-tailwindcss with-tailwindcss-app
-# or
-yarn create next-app --example with-tailwindcss with-tailwindcss-app
-# or
-pnpm create next-app -- --example with-tailwindcss with-tailwindcss-app
+git clone https://github.com/default-sameer/nextjs-typescript-tailwind.git                                                                                                                    
 ```
 
-Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
+### Install Dependecies
+
+```bash
+$ yarn install 
+or
+$ npm install
+```
+
+### Run the App
+
+```bash
+$ yarn dev
+or 
+$ npm run dev
+```
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Next App with Typescript & Tailwind</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold text-head">
           Welcome to{' '}
-          <a className="text-blue-600 relative after:absolute after:bg-button after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-left after:scale-x-100 hover:after:origin-bottom-right hover:after:scale-x-0 after:transition-transform after:ease-in-out after:duration-300" href="https://nextjs.org">
+          <a className="text-blue-600 relative after:absolute after:bg-black after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-left after:scale-x-100 hover:after:origin-bottom-right hover:after:scale-x-0 after:transition-transform after:ease-in-out after:duration-300" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
@@ -26,18 +26,6 @@ const Home: NextPage = () => {
         </p>
           
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center">
-        <a
-          className="flex items-center justify-center gap-2 text-paragraph"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
